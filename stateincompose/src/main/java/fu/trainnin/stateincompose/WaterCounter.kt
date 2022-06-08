72861d50c91dc432b9a82dc9e39bfd9dae68caa9
fu_chen <tec45e6t@gmail.com>
2022-06-08 23:53:43 +0800

    feat: 4.Events in Compose

--- stateincompose/src/main/java/fu/trainnin/stateincompose/WaterCounter.kt
+++ stateincompose/src/main/java/fu/trainnin/stateincompose/WaterCounter.kt
@@ -1,6 +1,8 @@
 package fu.trainnin.stateincompose
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,11 +11,15 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    var count = 0
+    Column(modifier.padding(16.dp)) {
+        Text(text = "You've had $count glasses.")
+        Button(onClick = {
+            count++
+        }, modifier = Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
 
 
