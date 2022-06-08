5931dd654dfd5fb234e2d2a2c5fdaca2c1533957
fu_chen <tec45e6t@gmail.com>
2022-06-09 00:17:31 +0800

    feat: 5.Memory in a composable function

--- stateincompose/src/main/java/fu/trainnin/stateincompose/WaterCounter.kt
+++ stateincompose/src/main/java/fu/trainnin/stateincompose/WaterCounter.kt
@@ -4,15 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
     Column(modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
         Text(text = "You've had $count glasses.")
         Button(onClick = {
             count++
