6170f09a2e4c6b583c47a58f57f8bdfd7e25aff2
fu_chen <tec45e6t@gmail.com>
2022-06-09 21:21:06 +0800

    feat: 11.Observable MutableList

--- stateincompose/src/main/java/fu/trainnin/stateincompose/WellnessScreen.kt
+++ stateincompose/src/main/java/fu/trainnin/stateincompose/WellnessScreen.kt
@@ -2,6 +2,8 @@ package fu.trainnin.stateincompose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fu.trainnin.stateincompose.ui.theme.AndroidPlaygroundTheme
@@ -10,9 +12,12 @@ import fu.trainnin.stateincompose.ui.theme.AndroidPlaygroundTheme
 fun WellnessScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
 
 @Preview
 @Composable
